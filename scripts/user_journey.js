db4ac566cc7b0cb6c03a51aac3602446ed10062b
amiraelbahrawy99 <amira.elbahrawy@yassir.com>
2026-04-20 13:00:21 +0200

    Update CSV Data

--- scripts/user_journey.js
+++ scripts/user_journey.js
@@ -3,13 +3,15 @@ import { login } from "../modules/auth.js";
 import http, { request } from "k6/http";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
-import { SharedArray } from 'k6/data'; 
-import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js'; 
+import { SharedArray } from "k6/data";
+import papaparse from "https://jslib.k6.io/papaparse/5.1.1/index.js";
 
-
-
-const csvData = new SharedArray('users', function () {
-  return papaparse.parse(open('../data/users.csv'), { header: true }).data;
+const csvData = new SharedArray("users", function () {
+  const data = papaparse.parse(open("../data/users.csv"), {
+    header: true,
+    skipEmptyLines: true,
+  }).data;
+  return data;
 });
 
 export const options = {
@@ -19,15 +21,16 @@ export const options = {
     { duration: "1m", target: 0 },
   ],
   thresholds: {
-    "http_req_duration{name:login}": ["p(95)<1000"],
-    "http_req_duration{name:menu}": ["p(95)<1000"],
+    "http_req_duration{name:login}": ["p(95)<2000"],
+    "http_req_duration{name:menu}": ["p(95)<3000"],
     "http_req_failed{name:login}": ["rate<0.01"],
     "http_req_failed{name:menu}": ["rate<0.01"],
   },
 };
 
 export default function () {
-    const user = csvData[__VU % csvData.length];
+  const user = csvData[(__VU - 1) % csvData.length];
+
   const baseURl = "https://food-preprod.yassir.io";
 
   let authData;
@@ -51,10 +54,11 @@ export default function () {
         authorization: authData.token,
       },
     };
-    const response = http.get(getMenuURL, {... params ,tags:{name:"menu"}});
-// console.log(response.body);
+    const response = http.get(getMenuURL, {
+      ...params,
+      tags: { name: "menu" },
+    });
 
-// console.log(request.body)
     check(response, {
       "Getting Menu status is 200": (r) => r.status === 200,
       "Menu has content": (r) => r.body.length > 0,
